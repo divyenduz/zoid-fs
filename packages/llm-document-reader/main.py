2caad0ab7f663b67bb5e1767dc26211f8d8fe7eb
Divyendu Singh <mail@divyendusingh.com>
2023-11-10 14:00:19 +0100

    chore: add pylint

--- packages/llm-document-reader/main.py
+++ packages/llm-document-reader/main.py
@@ -1,5 +1,9 @@
+"""
+Script to extract document (pdf) metadata from a PDF file (supports pdfs with images)
+"""
 import sqlite3
 import pickle
+import sys
 from langchain.prompts import ChatPromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.embeddings import OpenAIEmbeddings
@@ -12,31 +16,31 @@ from pdf_loader import PDFLoader
 conn = sqlite3.connect("packages/sqlite-backend/prisma/fs.db")
 db = conn.cursor()
 r = db.execute(
-    f"SELECT * FROM MetaData WHERE status = 'PENDING' AND fileType = 'application/pdf'"
+    "SELECT * FROM MetaData WHERE status = 'PENDING' AND fileType = 'application/pdf'"
 ).fetchone()
 if not r:
     print("No pending PDFs")
-    exit(0)
-(id, status, file_type, title, translation, date, embeddings, file_id) = r
-print(id, status, file_type, title, date, file_id)
+    sys.exit(0)
+(_id, status, file_type, title, translation, date, embeddings, file_id) = r
+print(_id, status, file_type, title, date, file_id)
 pdf_bytes_r = db.execute(
     f"SELECT content FROM Content WHERE fileId = {file_id} ORDER BY offset ASC"
 ).fetchall()
 print("bytes len:", len(pdf_bytes_r))
-pdf_bytes = b"".join(row[0] for row in pdf_bytes_r)
-pdf_loader = PDFLoader(pdf_bytes)
+PDF_BYTES = b"".join(row[0] for row in pdf_bytes_r)
+pdf_loader = PDFLoader(PDF_BYTES)
 pages = pdf_loader.open_pdf()
-pages_text = "".join([page.page_content for page in pages])
-print("text len:", len(pages_text))
-vectorstore = FAISS.from_texts([pages_text], embedding=OpenAIEmbeddings())
+PAGES_TEXT = "".join([page.page_content for page in pages])
+print("text len:", len(PAGES_TEXT))
+vectorstore = FAISS.from_texts([PAGES_TEXT], embedding=OpenAIEmbeddings())
 embeddings = pickle.dumps(vectorstore)
 
-template = """Answer the question based only on the following document:
+TEMPLATE = """Answer the question based only on the following document:
 {document}
 
 Question: {question}
 """
-prompt = ChatPromptTemplate.from_template(template)
+prompt = ChatPromptTemplate.from_template(TEMPLATE)
 
 retriever = vectorstore.as_retriever()
 model = ChatOpenAI()
@@ -49,15 +53,24 @@ chain = (
 )
 
 date = chain.invoke(
-    "What's the date of the document? Respond with just the date in ISO 8601 format no other text, if there is no date return None."
+    """What's the date of the document?
+    Respond with just the date in ISO 8601 format no other text, 
+    if there is no date return None.
+    """
 )
 
 title = chain.invoke(
-    "What's the subject of the document? Respond with just the subject and no other text, if there is no subject create one based on the text in English."
+    """What's the subject of the document? 
+    Respond with just the subject and no other text,
+    if there is no subject create one based on the text in English.
+    """
 )
 
 translation = chain.invoke(
-    "Translate the document to English, if it is not already in English? If it is in English, return an empty string."
+    """Translate the document to English, 
+    if it is not already in English? 
+    If it is in English, return an empty string.
+    """
 )
 
 db.execute(
--- packages/llm-document-reader/pdf_loader.py
+++ packages/llm-document-reader/pdf_loader.py
@@ -1,3 +1,6 @@
+"""
+PDFLoader: Load a PDF as text or vectorstore
+"""
 import tempfile
 from langchain.document_loaders import PyPDFLoader
 from langchain.vectorstores import FAISS
@@ -5,22 +8,30 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 
 
 class PDFLoader:
+    """
+    PDFLoader: Load a PDF as text or vectorstore
+    """
+
     def __init__(self, pdf_path_or_bytes):
-        if type(pdf_path_or_bytes) == str:
-            self.loader = PyPDFLoader(self.pdf_path_or_bytes, extract_images=True)
-        elif type(pdf_path_or_bytes) == bytes:
-            file_like = tempfile.NamedTemporaryFile(delete=False)
-            file_like.write(pdf_path_or_bytes)
-            file_like.seek(0)
-            self.loader = PyPDFLoader(file_like.name, extract_images=True)
+        if isinstance(pdf_path_or_bytes, str):
+            self.loader = PyPDFLoader(pdf_path_or_bytes, extract_images=True)
+        elif isinstance(pdf_path_or_bytes, bytes):
+            with tempfile.NamedTemporaryFile(delete=False) as file_like:
+                file_like.write(pdf_path_or_bytes)
+                file_like.seek(0)
+                self.loader = PyPDFLoader(file_like.name, extract_images=True)
         else:
-            raise Exception("Invalid pdf_path / pdf_bytes")
+            raise ValueError(
+                "Invalid argument: pdf_path_or_bytes must be a string or bytes."
+            )
 
     def open_pdf(self):
+        """Open a PDF and return a list of pages"""
         pages = self.loader.load()
         return pages
 
     def vector_store(self):
+        """Open a PDF and return a vector store"""
         pages = self.open_pdf()
         vectorstore = FAISS.from_documents(pages, embedding=OpenAIEmbeddings())
         return vectorstore
